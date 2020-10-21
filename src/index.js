6a419f2d47aab061d4eec625e4eafb5ae0edd538
Richard Creedon <richardcreedon1989@gmail.com>
2020-10-21 18:53:00 +0100

    another commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux"; 
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux"; //last 2 required for dev tools/middleware
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //required for devtools
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+
 ReactDOM.render(
 <Provider store={store}> 
 <App />
